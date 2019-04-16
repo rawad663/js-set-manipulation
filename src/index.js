8ead13cf1945d51966f9ebd206ac3c19bf056066
rawad663 <rawad.karam663@gmail.com>
2019-04-15 20:01:08 -0400

    added difference in set difference

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const {
-    intersection
-} = require('./set-manipulation');
+    intersection,
+    union,
+    difference
+} = require('./set');
 
-const set1 = [1, 2, 3, 4];
-const set2 = [2];
-const set3 = [3];
+const set1 = [1, 2, 3, 4, 6];
+const set2 = [1, 2];
+const set3 = [4];
 
+console.log(difference(set1, [set2, set3]));
 
 module.exports = {
     set: {
         intersection,
         union,
         difference
-    },
-    prob: {
-        
     }
 };
--- src/set/index.js
+++ src/set/index.js
@@ -2,12 +2,30 @@ const intersection = (...args) => {
     let [arg1, ...rest] = args;
 
     rest.forEach(arg => {
-        arg1 = arg1.filter(el => arg.includes(el))
+        arg1 = arg1.filter((el, index) => (
+            arg.includes(el) && arg1.indexOf(el) === index)
+        )
     })
 
     return arg1;
 } 
 
+const union = (...args) => {
+    let [arg1, arg2, ...rest] = args;
+
+    const merged = arg2.concat(...rest);
+    return arg1.filter((el, index) => merged.includes(el) && arg1.indexOf(el) === index);
+}
+
+const difference = (set, subtractionSets) => {
+    const [ arg1, rest ] = subtractionSets;
+    const merged = arg1.concat(...rest);
+
+    return set.filter(el => !merged.includes(el));
+}
+
 module.exports = {
-    intersection
+    intersection,
+    union,
+    difference
 }
