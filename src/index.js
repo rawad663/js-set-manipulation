c6ed278bacb44341dcfb86abde292bc6097a3fa6
rawad663 <rawad.karam663@gmail.com>
2019-04-15 22:30:37 -0400

    removed console log

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const setFunctions = require('./set');
 const probFunctions = require('./prob');
 
-console.log(probFunctions.PrFair(['H', 'T'], 4))
+// console.log(probFunctions.PrFair(['H', 'T'], 4))
 
 module.exports = {
     set: {
